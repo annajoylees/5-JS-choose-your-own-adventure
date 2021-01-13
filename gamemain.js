bd04d472aa06574cbf0301373dc841ffd95b01d6
Anna Joy Lees <annajoylees@gmail.com>
2021-01-13 16:53:04 -0500

    new game updates

--- gamemain.js
+++ gamemain.js
@@ -2,8 +2,8 @@ let isAlive = true
 let hp = 20
 let checkStatus = null
 
-if (hp === 0) {
-    isAlive === false
+if (hp = 0) {
+    isAlive = false
 }
 
 checkStatus = confirm('Vault 111 is now open! You have 20 HP points.')
@@ -13,54 +13,55 @@ let orderChoice = prompt('Do you want to A. Find a weapon or B. Cry out for Shau
 if (orderChoice === 'A') {
     alert('You found a Pipe Pistol!')
     hp = 20
-    isAlive === true 
+    isAlive = true 
     
 }
     
     else if (orderChoice === 'B') {
         alert('A nearby raider heard you and attacked. You died.')
         hp = 0
-        isAlive === false
+        isAlive = false
        
     }
 
-if(isAlive === true) {
+if(isAlive = true) {
     orderChoice = prompt('Would you like to A. Search for survivors or B. Explore alone?')
 }
 if (orderChoice === 'A') {
     alert('You have discovered Codsworth at home. You are safe.')
-    isAlive === true
+    isAlive = true
 }
     else if (orderChoice === 'B') {
         alert('You walked a while and discovered Concord. +10 RADS.')
         hp = 10
-        isAlive === true
+        isAlive = true
     
         
     }
 
-if ((isAlive === true) && (hp = 10)) {
+if ((isAlive = true) && (hp = 10)) {
     checkStatus = confirm('You have 10 HP points remaining. Find RadAway.')
-    if (checkStatus === true) {
+    if (checkStatus = true) {
         orderChoice = prompt('Would you like to A. Find RadAway or B. Take your chances and keep exploring?')
         if (orderChoice === 'A'){
             hp = 20
-            isAlive === true
+            isAlive = true
             alert('Your RADS have decreased! HP is restored to 20.')
         }
 
         else if (orderChoice === 'B') {
             hp = 0
-            isAlive === false
+            isAlive = false
+            checkStatus = confirm('You have died of radiation poisoning and lost the game. Goodbye.')
         }
     }
 }
 
-if ((hp === 0) && (isAlive === false)) {
-    checkStatus = confirm('You have lost the game. Goodbye.')
-}
+// if ((hp = 0) && (isAlive = false)) {
+//     checkStatus = confirm('You have lost the game. Goodbye.')
+// }
 
-if ((hp === 20) && (isAlive === true)) {
+if (isAlive = true) {
     alert('You have found shelter for the night.')
     checkStatus = confirm('You are safe and have won the game! Goodbye.')
 }
